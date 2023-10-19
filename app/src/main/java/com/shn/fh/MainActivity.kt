e02cf2c345bf696b5d4cd579ea560f42043ae32a
Shaheen Shabib <shaheen.s@eteamindia.com>
2023-10-19 17:43:36 +0530

    location permission added get latlong added

--- app/src/main/java/com/shn/fh/MainActivity.kt
+++ app/src/main/java/com/shn/fh/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.shn.fh
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.shn.fh.utils.Utils
 
 class MainActivity : AppCompatActivity() {
+
+    private val locationReqId: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ask for location permission
+        if (!Utils.checkIfAccessFineLocationGranted(this)) {
+            Utils.requestLocationPermission(
+                this, locationReqId
+            )
+        }
+
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            locationReqId -> {
+                if (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    Utils.getLatLong(this@MainActivity) { latitude, longitude ->
+                        Toast.makeText(this@MainActivity, "loc: $latitude:: $longitude",Toast.LENGTH_LONG).show()
+
+                    }
+
+
+                } else {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.msg_location_permission_required),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/shn/fh/utils/Utils.kt
@@ -0,0 +1,70 @@
+package com.shn.fh.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+import com.shn.fh.R
+
+class Utils {
+
+    companion object {
+
+        fun requestLocationPermission(activity: AppCompatActivity, requestId: Int) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                requestId
+            )
+        }
+
+        fun checkIfAccessFineLocationGranted(context: Context): Boolean {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            } else {
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.msg_location_permission_required),
+                    Toast.LENGTH_LONG
+                ).show()
+                return false
+            }
+        }
+
+         fun getLatLong(context: Context, callback: (Double, Double) -> Unit) {
+            if (checkIfAccessFineLocationGranted(context)) {
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        if (location != null) {
+                            val latitude = location.latitude
+                            val longitude = location.longitude
+                            callback(latitude, longitude)
+                        } else {
+                            // Location is null, handle the case
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        // Handle failure
+                    }
+            }
+        }
+
+
+    }
+
+}
+
+
